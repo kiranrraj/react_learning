42a6bffd8e853ca898a5ce439a3ffdf6a9b434a9
Kiran Raj R <kiranrajvjd@gmail.com>
2025-06-02 17:48:51 +0530

    P0 -- Accordion V2

--- P0_tutorial/P0_1_accordion/accordion-app/src/Accordion.jsx
+++ P0_tutorial/P0_1_accordion/accordion-app/src/Accordion.jsx
@@ -2,25 +2,87 @@ import { useState } from "react";
 import myData from "./data";
 
 export default function Accordion() {
-  const [answer, setAnswer] = useState(null);
+  const [openid, setOpenid] = useState(null);
+  const [multiselect, setMultiselect] = useState(false);
+  const [multiIds, setMultiIds] = useState([]);
+  let isCurrentAnswerVisible;
 
+  // Function to display answers based on status of multiselect and card selected
+  function showAnswer(id, ans) {
+    if (multiselect) {
+      if (multiIds.includes(id)) {
+        return <h3 className="answer">{ans}</h3>;
+      } else {
+        return null;
+      }
+    } else {
+      if (openid === id) {
+        return <h3 className="answer">{ans}</h3>;
+      } else {
+        return null;
+      }
+    }
+  }
+
+  // Set active cards
   const show = (id) => {
-    // If the clicked ID is already the one in state, set it to null (hide it)
-    // Otherwise, set it to the new ID (show it)
-    setAnswer(answer === id ? null : id);
+    if (multiselect) {
+      if (multiIds.includes(id)) {
+        setMultiIds(multiIds.filter((item) => item !== id));
+      } else {
+        setMultiIds([...multiIds, id]);
+      }
+    } else {
+      if (!(openid == id)) {
+        setOpenid(id);
+      } else {
+        setOpenid(null);
+      }
+    }
+  };
+
+  // Logic to check if the card is open or not
+  function answerStatus(id) {
+    if (!multiselect) {
+      isCurrentAnswerVisible = openid === id;
+    } else {
+      isCurrentAnswerVisible = multiIds.includes(id);
+    }
+    return isCurrentAnswerVisible;
+  }
+
+  // Handle multiselect
+  const handleMultiselect = (status) => {
+    if (!multiselect) {
+      setMultiIds([]);
+      setOpenid(null);
+    }
+    setMultiselect(status);
   };
 
   return (
     <div className="container">
+      <div className="select">
+        <label htmlFor="multi">Multi Selection</label>
+        <input
+          type="checkbox"
+          checked={multiselect}
+          name="multi"
+          id="multi"
+          onChange={() => {
+            handleMultiselect(!multiselect);
+          }}
+        />
+      </div>
       {myData.map((data) => {
-        const isCurrentAnswerVisible = answer === data.id;
         return (
           <div className="card" key={data.id}>
             <button className="showBtn" onClick={() => show(data.id)}>
-              {isCurrentAnswerVisible ? "Hide" : "Show"}
+              {/* Show button text */}
+              {answerStatus(data.id) ? "Hide" : "Show"}
             </button>
             <h2 className="question">{data.question}</h2>
-            {answer === data.id ? <h3 className="answer">{data.answer}</h3> : null}
+            {showAnswer(data.id, data.answer)}
           </div>
         );
       })}
