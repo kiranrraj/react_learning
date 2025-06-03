b180ce3466996ef3b7d710ab5a12db4607553560
Kiran Raj R <kiranrajvjd@gmail.com>
2025-06-03 16:17:25 +0530

    P0 -- Image Slider App V2

--- P0_tutorial/P0_4_image_slider/image-slider-app/src/SliderImage.jsx
+++ P0_tutorial/P0_4_image_slider/image-slider-app/src/SliderImage.jsx
@@ -1,43 +1,60 @@
-import { useState } from "react"
-import ImageURLs from "./image"
-
+import { useState } from "react";
+import ImageURLs from "./image";
+import { useEffect, useCallback } from "react";
 
 const ImageSlider = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const imageIndex = ImageURLs.length - 1;
 
-  const [currentIndex, setCurrentIndex] = useState(0)
-  const imageCount = ImageURLs.length - 1
-
-  
   const gotoPrev = () => {
-    if (currentIndex === 0 ) {
-        setCurrentIndex(imageCount - 1)
+    if (currentIndex === 0) {
+      setCurrentIndex(imageIndex);
     } else {
-        setCurrentIndex(currentIndex - 1)
+      setCurrentIndex(currentIndex - 1);
     }
-    console.log(currentIndex, imageCount)
-  }
+  };
 
-  const gotoNext = () => {
-    if (currentIndex >= imageCount) {
-        setCurrentIndex(0)
-    } else{
-        setCurrentIndex(currentIndex + 1)
+  const gotoNext = useCallback(() => {
+    if (currentIndex >= imageIndex ) {
+      setCurrentIndex(0);
+    } else {
+      setCurrentIndex(currentIndex + 1);
     }
-    console.log(currentIndex, imageCount)
-  }
+  }, [imageIndex, currentIndex]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      gotoNext();
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [gotoNext]);
 
   return (
     <div className="container">
-        <div className="wrapper">
-            <div className="imageContainer">
-                <img className="img" src={ImageURLs[currentIndex].url} alt="" />
-                <button className="btn btnPrev" onClick={gotoPrev}>Prev</button>
-                <button className="btn btnNext" onClick={gotoNext}>Next</button>
-            </div>
+      <div className="wrapper">
+        <div className="imageContainer">
+          <img className="img" src={ImageURLs[currentIndex].url} alt="" />
+          <button className="btn btnPrev" onClick={gotoPrev}>
+            Prev
+          </button>
+          <button className="btn btnNext" onClick={gotoNext}>
+            Next
+          </button>
         </div>
+        <div className="progress">
+          {[...Array(imageIndex + 1)].map((_, index) => {
+            return (
+              <span
+                key={index}
+                className={index <= currentIndex ? "dot active" : "dot"}
+              ></span>
+            );
+          })}
+        </div>
+      </div>
     </div>
-  )
-
-}
+  );
+};
 
 export default ImageSlider;
