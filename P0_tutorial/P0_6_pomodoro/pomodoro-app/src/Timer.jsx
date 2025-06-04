22bf6ab2f6c72f24ef81d7f907605cf9600619d7
Kiran Raj R <kiranrajvjd@gmail.com>
2025-06-04 20:26:00 +0530

    P0 -- Pomodoro-app V1 -- update

--- P0_tutorial/P0_6_pomodoro/pomodoro-app/src/Timer.jsx
+++ P0_tutorial/P0_6_pomodoro/pomodoro-app/src/Timer.jsx
@@ -3,60 +3,75 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 const Timer = () => {
+  const initialValue = 90000;
+  const [countDownMS, setCountDownMS] = useState(initialValue);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef(null);
 
-    const initialValue = 60000;
-    const [countDownMS, setCountDownMS] = useState(initialValue)
-    const [isRunning, setIsRunning] = useState(false)
-    const intervalRef = useRef(null);
+  function startHandler() {
+    setIsRunning(true);
+  }
 
-    function startHandler() {
-        setIsRunning(true);
-    }
+  function pauseHandler() {
+    setIsRunning(false);
+  }
 
-    function pauseHandler() {
-        setIsRunning(false);
-    }
+  function resetHandler() {
+    setIsRunning(false);
+    setCountDownMS(initialValue);
+  }
 
-    function resetHandler(){
-        setIsRunning(false);
-        setCountDownMS(initialValue);
-    }
-
-    function formatTime(ms) {
-    let timeInSec = (ms/ 1000)
-    let min = String(Math.floor(timeInSec / 60)).padStart(2, "0"); 
+  function formatTime(ms) {
+    let timeInSec = ms / 1000;
+    let min = String(Math.floor(timeInSec / 60)).padStart(2, "0");
     let sec = String(Math.floor(timeInSec % 60)).padStart(2, "0");
-    return `${min}:${sec}`
-}
+    return `${min}:${sec}`;
+  }
 
-    useEffect( () => {
+  useEffect(() => {
+    if (isRunning && countDownMS > 0) {
+      intervalRef.current = setInterval(() => {
+        setCountDownMS((prevCountDownMS) => prevCountDownMS - 1000);
+      }, 1000);
+    } else if (countDownMS <= 0) {
+      clearInterval(intervalRef.current);
+      setIsRunning(false);
+      setCountDownMS(0);
+      console.log("Countdown finished!");
+    }
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, [isRunning, countDownMS]);
 
-        if(isRunning && countDownMS > 0) {
-            intervalRef.current = setInterval( () => {
-                setCountDownMS(prevCountDownMS => prevCountDownMS - 1000 )
-            }, 1000)
-        } else if (countDownMS <= 0) {
-            clearInterval(intervalRef.current); 
-            setIsRunning(false);    
-            setCountDownMS(0);
-            console.log("Countdown finished!");
-        }
-        return () => {
-            clearInterval(intervalRef.current)
-        }
-    }, [isRunning, countDownMS])
-
-    return(
-        <div className="timer_container">
-            <div className="timer">{formatTime(countDownMS)}</div>
-            <div className="btn_container">
-                <button className="btn start" onClick={startHandler} disabled={isRunning}>Start</button>
-                <button className="btn stop" onClick={pauseHandler} disabled={!isRunning}>Pause</button>
-                <button className="btn reset" onClick={resetHandler} disabled={!isRunning}>Reset</button>
-            </div>
-
-        </div>
-    )
-}
+  return (
+    <div className="timer_container">
+      <div className="timer">{formatTime(countDownMS)}</div>
+      <div className="btn_container">
+        <button
+          className="btn start"
+          onClick={startHandler}
+          disabled={isRunning}
+        >
+          Start
+        </button>
+        <button
+          className="btn stop"
+          onClick={pauseHandler}
+          disabled={!isRunning}
+        >
+          Pause
+        </button>
+        <button
+          className="btn reset"
+          onClick={resetHandler}
+          disabled={!isRunning}
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Timer;
